f5b3a7042325f8928df4b98eed9a5126a5a6f800
tdora28 <tokaidora88@gmail.com>
2024-02-07 17:43:27 +0200

    Add my solution for search filter

--- src/routes/Countries.jsx
+++ src/routes/Countries.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Spinner } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
@@ -6,6 +6,7 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
 import { useDispatch, useSelector } from 'react-redux';
 import { initializeCountries } from '../store/countriesSlice';
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -20,6 +21,14 @@ const Countries = () => {
 
   const countriesList = useSelector((state) => state.countries.countries);
   const loading = useSelector((state) => state.countries.isLoading);
+
+  const [search, setSearch] = useState('');
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCountries = countriesList.filter((country) => country.name.common.toLowerCase().includes(search.toLowerCase()));
 
   useEffect(() => {
     dispatch(initializeCountries());
@@ -36,44 +45,52 @@ const Countries = () => {
   }
 
   return (
-    <Container fluid>
-      <Row xs={2} md={3} lg={4} className=" g-3">
-        {countriesList.map((country) => (
-          <Col key={country.name.official} className="mt-5">
-            <Card className="h-100">
-              <FavoriteIcon color="red" onClick={() => dispatch(addFavourite(country))} />
-              <Card.Img
-                variant="top"
-                className="rounded h-50"
-                src={country.flags.png}
-                style={{
-                  objectFit: 'cover',
-                  minHeight: '200px',
-                  maxHeight: '200px',
-                }}
-              />
-              <Card.Body className="d-flex flex-column">
-                <Card.Title>{country.name.common}</Card.Title>
-                <Card.Subtitle className="mb-5 text-muted">{country.name.official}</Card.Subtitle>
-                <ListGroup variant="flush" className="flex-grow-1 justify-content-end">
-                  <ListGroup.Item>
-                    <i className="bi bi-translate me-2"></i>
-                    {Object.values(country.languages ?? {}).join(', ')}
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <i className="bi bi-cash-coin me-2"></i>
-                    {Object.values(country.currencies || {})
-                      .map((currency) => currency.name)
-                      .join(', ')}
-                  </ListGroup.Item>
-                  <ListGroup.Item>{country.population.toLocaleString()}</ListGroup.Item>
-                </ListGroup>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <>
+      <Form.Control style={{ width: '500px', margin: '0 auto' }} size="lg" type="text" placeholder="Search countries..." onChange={handleSearch} />
+
+      <Container fluid>
+        <Row xs={2} md={3} lg={4} className=" g-3">
+          {search && filteredCountries.length === 0 ? (
+            <p>No matching country found!</p>
+          ) : (
+            filteredCountries.map((country) => (
+              <Col key={country.name.official} className="mt-5">
+                <Card className="h-100">
+                  <FavoriteIcon color="red" onClick={() => dispatch(addFavourite(country))} />
+                  <Card.Img
+                    variant="top"
+                    className="rounded h-50"
+                    src={country.flags.png}
+                    style={{
+                      objectFit: 'cover',
+                      minHeight: '200px',
+                      maxHeight: '200px',
+                    }}
+                  />
+                  <Card.Body className="d-flex flex-column">
+                    <Card.Title>{country.name.common}</Card.Title>
+                    <Card.Subtitle className="mb-5 text-muted">{country.name.official}</Card.Subtitle>
+                    <ListGroup variant="flush" className="flex-grow-1 justify-content-end">
+                      <ListGroup.Item>
+                        <i className="bi bi-translate me-2"></i>
+                        {Object.values(country.languages ?? {}).join(', ')}
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <i className="bi bi-cash-coin me-2"></i>
+                        {Object.values(country.currencies || {})
+                          .map((currency) => currency.name)
+                          .join(', ')}
+                      </ListGroup.Item>
+                      <ListGroup.Item>{country.population.toLocaleString()}</ListGroup.Item>
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          )}
+        </Row>
+      </Container>
+    </>
   );
 };
 
