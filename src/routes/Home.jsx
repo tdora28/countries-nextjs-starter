1b1d6ac9c308f15b44ba084443aa8a5be8c415eb
tdora28 <tokaidora88@gmail.com>
2024-03-13 10:42:01 +0200

    Add Register styles and fix layout issues in Header and Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,9 +3,9 @@ import Image from 'react-bootstrap/Image';
 
 const Home = () => {
   return (
-    <Container style={{ minHeight: 'calc(100vh - 80px)' }} className="d-flex flex-column align-items-center justify-content-center gap-5">
+    <Container className="d-flex flex-column align-items-center justify-content-center gap-5" style={{ minHeight: 'calc(100vh - 70px)' }}>
       <h1 className="display-1 text-center">Countries of the World</h1>
-      <Image src="../src/assets/images/galaxy-map.png" fluid />
+      <Image src="../src/assets/images/galaxy-map.png" style={{ maxWidth: '1000px', width: '100%' }} />
     </Container>
   );
 };
