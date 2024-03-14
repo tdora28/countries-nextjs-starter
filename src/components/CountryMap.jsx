945c9828f56372e2eadd52a5d326561ab64d404c
tdora28 <tokaidora88@gmail.com>
2024-03-14 10:51:39 +0200

    Fix map zoom

--- src/components/CountryMap.jsx
+++ src/components/CountryMap.jsx
@@ -4,7 +4,7 @@ import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 const CountryMap = ({ latitude, longitude }) => {
   return (
     <LoadScript googleMapsApiKey={import.meta.env.VITE_MAP_API}>
-      <GoogleMap mapContainerStyle={{ height: '400px', width: '100%' }} zoom={5} center={{ lat: latitude, lng: longitude }}>
+      <GoogleMap mapContainerStyle={{ height: '400px', width: '100%' }} zoom={6} center={{ lat: latitude, lng: longitude }}>
         <Marker position={{ lat: latitude, lng: longitude }} />
       </GoogleMap>
     </LoadScript>
