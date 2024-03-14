31cb4bed12853ed537464c1819a6bb19c4513bcb
tdora28 <tokaidora88@gmail.com>
2024-03-14 11:27:22 +0200

    Add styles to Favourites

--- src/routes/Favourites.jsx
+++ src/routes/Favourites.jsx
@@ -1,15 +1,17 @@
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { useEffect } from 'react';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Row from 'react-bootstrap/Row';
 import { useDispatch, useSelector } from 'react-redux';
 import { getFavouritesFromSource } from '../auth/firebase';
 import { initializeCountries } from '../store/countriesSlice';
 import { addFavourite, removeFavourite } from '../store/favouritesSlice';
+import { Link } from 'react-router-dom';
+
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 const Favourites = () => {
   const dispatch = useDispatch();
@@ -29,38 +31,36 @@ const Favourites = () => {
   }, [dispatch]);
 
   return (
-    <Container fluid>
-      <Row xs={2} md={3} lg={4} className=" g-3">
+    <Container className="my-5">
+      <Row xs={1} sm={2} md={3} lg={4} className="g-3">
         {countriesList.map((country) => (
-          <Col key={country.name.official} className="mt-5">
-            <Card className="h-100">
-              {favourites.some((favourite) => favourite === country.name?.common) ? <FavoriteBorderIcon onClick={() => dispatch(removeFavourite(country.name.common))} /> : <FavoriteIcon onClick={() => dispatch(addFavourite(country.name.common))} />}
+          <Col className="mt-5" key={country.name.common}>
+            <Card className="h-100" border="info" bg="ligth" text="dark">
+              <Card.Header style={{ backgroundColor: '#0dcaf0', color: 'white' }}>
+                {favourites.some((favourite) => favourite === country.name?.common) ? (
+                  <FavoriteIcon style={{ display: 'block', marginLeft: 'auto', color: 'white' }} onClick={() => dispatch(removeFavourite(country.name.common))} />
+                ) : (
+                  <FavoriteBorderIcon style={{ display: 'block', marginLeft: 'auto' }} onClick={() => dispatch(addFavourite(country.name.common))} />
+                )}
+              </Card.Header>
+
               <Card.Img
-                variant="top"
-                className="rounded h-50"
+                variant="bottom"
                 src={country.flags.svg}
                 style={{
                   objectFit: 'cover',
-                  minHeight: '200px',
-                  maxHeight: '200px',
+                  height: '200px',
                 }}
               />
+
               <Card.Body className="d-flex flex-column">
                 <Card.Title>{country.name.common}</Card.Title>
                 <Card.Subtitle className="mb-5 text-muted">{country.name.official}</Card.Subtitle>
-                <ListGroup variant="flush" className="flex-grow-1 justify-content-end">
-                  <ListGroup.Item>
-                    <i className="bi bi-translate me-2"></i>
-                    {Object.values(country.languages ?? {}).join(', ')}
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <i className="bi bi-cash-coin me-2"></i>
-                    {Object.values(country.currencies || {})
-                      .map((currency) => currency.name)
-                      .join(', ')}
-                  </ListGroup.Item>
-                  <ListGroup.Item>{country.population.toLocaleString()}</ListGroup.Item>
-                </ListGroup>
+                <Link style={{ textDecoration: 'none', marginTop: 'auto' }} to={`/countries/${country.name.common}`} state={{ country: country }}>
+                  <Button className="d-block mx-auto" variant="info">
+                    Read More
+                  </Button>
+                </Link>
               </Card.Body>
             </Card>
           </Col>
