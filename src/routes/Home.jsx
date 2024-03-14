803a66252d97af2ae12b3eacde6fd153be65a2eb
tdora28 <tokaidora88@gmail.com>
2024-03-14 16:04:41 +0200

    Fix image not showing up in production build on Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,12 @@
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
+import galaxyMap from '../assets/galaxy-map.png';
 
 const Home = () => {
   return (
     <Container className="d-flex flex-column align-items-center justify-content-center gap-5" style={{ minHeight: 'calc(100vh - 70px)' }}>
       <h1 className="display-1 text-center">Countries of the World</h1>
-      <Image src="../src/assets/images/galaxy-map.png" style={{ maxWidth: '1000px', width: '100%' }} />
+      <Image src={galaxyMap} style={{ maxWidth: '1000px', width: '100%' }} />
     </Container>
   );
 };
